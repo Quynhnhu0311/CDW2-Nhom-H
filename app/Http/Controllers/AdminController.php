fc83f57dcf957d4ee48872ecf98cb8253ad12b44
DESKTOP-FKLAI7P\quyen <quyen.le@wsinteractive.com>
2022-11-22 13:28:18 +0700

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,32 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-<<<<<<< HEAD
-
-class AdminController extends Controller
-{
-    function login_admin(Request $request, $name = 'index'){
-        $admin_email = $request->email;
-        $admin_pass = md5($request->pass);
-        $result = DB::table('admins')->where('email',$admin_email)
-                                    ->where('password',$admin_pass)->first();
-        if($result) {
-            Session::put('name_admin',$result->name);
-            Session::put('id_admin',$result->id);
-            return Redirect::to('/');
-        }
-        else{
-            Session::put('message','Mật khẩu hoặc tài khoản bị sai. Vui lòng nhập lại!');
-            return Redirect::to('/login');
-        }
-    }
-    function logout_admin(Request $request) {
-        Session::put('name_admin',null);
-        Session::put('id_admin',null);
-        $request->session()->forget(['cart']);
-        $request->session()->forget(['coupon']);
-        return Redirect::to('/');
-=======
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
 use App\Models\Product;
@@ -413,6 +387,5 @@ class AdminController extends Controller
         Session::put('message_deleteCoupon','Xóa Coupon Thành Công!');
         return view('admin.coupons')->with('get_all_coupon', $get_all_coupon);
         //return Redirect::to('admin.coupons');
->>>>>>> 23f54db6cca757a60aa51fc30d680eb2f7b5b738
     }
 }
