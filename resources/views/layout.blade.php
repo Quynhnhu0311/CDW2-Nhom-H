3f923372f8a6d6859320c5dbfd9009a8b7e311be
Your Name <15911hth@gmail.com>
2022-11-28 22:44:57 +0700

    Update Code

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -126,17 +126,7 @@
                     <nav class="header__menu mobile-menu">
                         <ul>
                             <li class="active"><a href="/">Home</a></li>
-                            <li><a href="/tat-ca-san-pham">Shop</a>
-                            </li>
-                            <li><a href="#">Pages</a>
-                                <ul class="dropdown">
-                                    <li><a href="{{ url ('about')}}">About Us</a></li>
-                                    <li><a href="{{ url ('shop-details')}}">Shop Details</a></li>
-                                    <li><a href="{{ url ('shopping-cart')}}">Shopping Cart</a></li>
-                                    <li><a href="{{ url ('checkout')}}">Check Out</a></li>
-                                    <li><a href="{{ url('blog-details') }}">Blog Details</a></li>
-                                </ul>
-                            </li>
+                            <li><a href="/tat-ca-san-pham">Shop</a></li>
                             <li><a href="/blog">Blog</a></li>
                             <li><a href="{{ url ('contact') }}">Contacts</a></li>
                         </ul>
